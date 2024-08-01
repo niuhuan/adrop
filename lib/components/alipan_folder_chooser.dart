313bfdcf541f99f3f343a93b418eb5f8b87fba86
niuhuan <niuhuancn@outlook.com>
2024-08-01 18:07:04 +0800

    准备列出文件夹

--- lib/components/alipan_folder_chooser.dart
+++ lib/components/alipan_folder_chooser.dart
@@ -24,8 +24,80 @@ class _AlipanFolderChooserState extends State<AlipanFolderChooser> {
 
   @override
   Widget build(BuildContext context) {
+    var ffi = folderFileId();
     return Column(
-      children: [],
+      children: [
+        Expanded(
+          key: Key("AFC:$ffi"),
+          child: ItemList(
+            folderFileId: ffi,
+          ),
+        ),
+      ],
+    );
+  }
+
+  String folderFileId() {
+    if (_current.isEmpty) {
+      return "root";
+    }
+    return _current.last.fileId;
+  }
+}
+
+class ItemList extends StatefulWidget {
+  const ItemList({super.key, required this.folderFileId});
+
+  final String folderFileId;
+
+  @override
+  State<StatefulWidget> createState() => _ItemListState();
+}
+
+class _ItemListState extends State<ItemList> {
+  late Future<List<Node>> _itemsFuture = _loadItems();
+
+  Future<List<Node>> _loadItems() async {
+    return [];
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<Node>>(
+      future: _itemsFuture,
+      builder: (BuildContext context, AsyncSnapshot<List<Node>> snapshot) {
+        if (snapshot.hasError) {
+          return Text("Error: ${snapshot.error}");
+        }
+        if (snapshot.hasData) {
+          return ListView.builder(
+            itemCount: snapshot.requireData.length,
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                title: Text(snapshot.requireData[index].fileName),
+                onTap: () {
+                  setState(() {
+                    // TODO
+                  });
+                },
+              );
+            },
+          );
+        }
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
     );
   }
 }
