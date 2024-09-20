9e944f1372d0a9dadd7d9590b1ebd4b94a746f48
niuhuan <niuhuancn@outlook.com>
2024-09-20 11:47:10 +0800

    :construction_worker: 区分android和linux构建

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -7,5 +7,5 @@ mod data_obj;
 mod custom_crypto;
 mod common;
 mod single_instance;
-#[cfg(any(target_os = "linux", target_os = "macos", target_os = "windows"))]
+#[cfg(any(all(target_os = "linux", target_os = "android"), target_os = "macos", target_os = "windows"))]
 mod single;
