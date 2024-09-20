d83daab6fad778ba9f3bef8abeda7b137db4f643
niuhuan <niuhuancn@outlook.com>
2024-09-20 11:56:44 +0800

    :construction_worker: 区分nix（android,macos,ios,linux ）构建

--- rust/src/api/init.rs
+++ rust/src/api/init.rs
@@ -2,7 +2,7 @@ use crate::define::init_root;
 use flutter_rust_bridge::for_generated::anyhow;
 
 pub async fn init_path(local_path: String) -> anyhow::Result<()> {
-    #[cfg(any(all(target_os = "linux", target_os = "android"), target_os = "macos", target_os = "windows"))]
+    #[cfg(any(target_os = "linux", target_os = "macos", target_os = "windows"))]
     crate::single::single().await;
     init_root(local_path.as_str()).await
 }
